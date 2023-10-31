6eb113a356bc74304252740ad94623bf6d397b50
arilibbetardanaputra <arilardana111@gmail.com>
2023-10-31 20:02:08 +0700

    push & commit final

--- ArrayNilai05.java
+++ ArrayNilai05.java
@@ -5,13 +5,15 @@ public class ArrayNilai05 {
         int[] nilaiakhir = new int[10];
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiakhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiakhir[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiakhir[i]);
+        for (int i = 0; i < nilaiakhir.length; i++) {
+            if (nilaiakhir[i] > 70) {System.out.println("Mahasiswa ke-" + i + " Lulus! ");
+            } else {System.out.println("Mahasiswa ke-" + i + " Tidak Lulus! " );
+        }
         }
     }
 }
